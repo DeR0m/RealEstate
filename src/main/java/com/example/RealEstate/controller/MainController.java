0e57b12b4439a0782f1fa6d6943d075aada2cc60
DeR0m <denisromantsov@mail.ru>
2021-02-20 20:18:14 +0300

    Add file uploading and static files serving

--- src/main/java/com/example/RealEstate/controller/MainController.java
+++ src/main/java/com/example/RealEstate/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -22,45 +23,6 @@ public class MainController {
     public String index(Map<String, Object> model) {
         return "index";
     }
-
-    @GetMapping("/main")
-    public String main(@RequestParam(required = false) String filter, Model model) {
-        Iterable<Message> messages = messageRepo.findAll();
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.addAttribute("messages", messages);
-        model.addAttribute("filter", filter);
-
-        return "main";
-    }
-
-    @PostMapping("/main")
-    public String add(
-            @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag,
-            Model model
-    ){
-        Message message = new Message(text, tag, user);
-
-        messageRepo.save(message);
-
-        Iterable<Message> messages = messageRepo.findAll();
-
-        model.addAttribute("messages", messages);
-
-        return "main";
-    }
-
-//    @PostMapping("delete")
-//    public String delete(@RequestParam String delete, Map<String, Object> model){
-//
-//    }
 
 
 }
