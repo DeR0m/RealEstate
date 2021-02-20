52abe3f9e42b5e60b59f1f6347ea60e1a739d97f
DeR0m <denisromantsov@mail.ru>
2021-02-20 20:21:12 +0300

    Add file uploading and static files serving

--- src/main/java/com/example/RealEstate/controller/MainController.java
+++ src/main/java/com/example/RealEstate/controller/MainController.java
@@ -1,16 +1,9 @@
 package com.example.RealEstate.controller;
 
-import com.example.RealEstate.domain.Message;
-import com.example.RealEstate.domain.User;
 import com.example.RealEstate.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
