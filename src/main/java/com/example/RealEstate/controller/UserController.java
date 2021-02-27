6e11ba87d3bb49edd8d7d25ee8b9e4b085d8cd63
DeR0m <denisromantsov@mail.ru>
2021-02-27 07:03:45 +0300

    Init user

--- src/main/java/com/example/RealEstate/controller/UserController.java
+++ src/main/java/com/example/RealEstate/controller/UserController.java
@@ -1,61 +1,44 @@
 package com.example.RealEstate.controller;
 
-import com.example.RealEstate.domain.Role;
 import com.example.RealEstate.domain.User;
-import com.example.RealEstate.repo.UserRepo;
+import com.example.RealEstate.exceptions.UserAlreadyExistException;
+import com.example.RealEstate.exceptions.UserNotFoundException;
 import com.example.RealEstate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
+
     @Autowired
-    private UserRepo userRepo;
-
-    @GetMapping
-    public String userList(Model model){
-        model.addAttribute("users", userRepo.findAll());
-        return "userList";
-    }
-
-    @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model){
-        model.addAttribute("user", user);
-        model.addAttribute("roles", Role.values());
-        return "userEdit";
-    }
+    private UserService userService;
 
     @PostMapping
-    public String userSave(
-            @RequestParam String username,
-            @RequestParam Map<String, String> form,
-            @RequestParam("userId") User user
-    ){
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()){
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
+    public ResponseEntity registration(@RequestBody User user){
+        try{
+            userService.registration(user);
+            return ResponseEntity.ok("Пользователь успешно сохранен");
         }
+        catch (UserAlreadyExistException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (Exception e){
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
 
-        userRepo.save(user);
-        return "redirect:/user";
+    @GetMapping
+    public ResponseEntity getOneUser(@RequestParam Long id){
+        try{
+            return ResponseEntity.ok(userService.getOne(id));
+        }
+        catch (UserNotFoundException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
     }
 }
--- src/main/java/com/example/RealEstate/service/UserService.java
+++ src/main/java/com/example/RealEstate/service/UserService.java
@@ -1,20 +1,31 @@
 package com.example.RealEstate.service;
 
+import com.example.RealEstate.domain.User;
+import com.example.RealEstate.exceptions.UserAlreadyExistException;
+import com.example.RealEstate.exceptions.UserNotFoundException;
 import com.example.RealEstate.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepo.findByUsername(username);
-    }
+   public User registration(User user) throws UserAlreadyExistException {
+       if(userRepo.findByUsername(user.getUsername()) != null){
+           throw new UserAlreadyExistException("Пользователь с таким именем существует");
+       }
+       return userRepo.save(user);
+   }
+
+   public User getOne(Long id) throws UserNotFoundException {
+        User user = userRepo.findById(id).get();
+        if(user == null){
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        return user;
+   }
 }
